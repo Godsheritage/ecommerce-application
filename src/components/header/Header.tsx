5b423f33ec7f5784b2233097fa3b0e0fe8e9babc
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-16 06:37:45 +0100

    fixed image import issue

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,79 +1,118 @@
-import React from 'react'
+import React from "react";
+import {FaUser, FaCartPlus, FaSearch} from 'react-icons/fa';
+import * as Logo from "../../assets/img/logo.png"
 
 const Header = () => {
   return (
     <div>
-      	<header className="header-section">
-		<div className="header-top">
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-2 text-center text-lg-left">
-						{/* <!-- logo --> */}
-						<a href="./index.html" className="site-logo">
-							<img src="img/logo.png" alt=""/>
-						</a>
-					</div>
-					<div className="col-xl-6 col-lg-5">
-						<form className="header-search-form">
-							<input type="text" placeholder="Search on divisima ...."/>
-							<button><i className="flaticon-search"></i></button>
-						</form>
-					</div>
-					<div className="col-xl-4 col-lg-5">
-						<div className="user-panel">
-							<div className="up-item">
-								<i className="flaticon-profile"></i>
-								<a href="#">Sign</a> In or <a href="#">Create Account</a>
-							</div>
-							<div className="up-item">
-								<div className="shopping-card">
-									<i className="flaticon-bag"></i>
-									<span>0</span>
-								</div>
-								<a href="#">Shopping Cart</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<nav className="main-navbar">
-			<div className="container">
-				{/* <!-- menu --> */}
-				<ul className="main-menu">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Women</a></li>
-					<li><a href="#">Men</a></li>
-					<li><a href="#">Jewelry
-						<span className="new">New</span>
-					</a></li>
-					<li><a href="#">Shoes</a>
-						<ul className="sub-menu">
-							<li><a href="#">Sneakers</a></li>
-							<li><a href="#">Sandals</a></li>
-							<li><a href="#">Formal Shoes</a></li>
-							<li><a href="#">Boots</a></li>
-							<li><a href="#">Flip Flops</a></li>
-						</ul>
-					</li>
-					<li><a href="#">Pages</a>
-						<ul className="sub-menu">
-							<li><a href="./product.html">Product Page</a></li>
-							<li><a href="./category.html">Category Page</a></li>
-							<li><a href="./cart.html">Cart Page</a></li>
-							<li><a href="./checkout.html">Checkout Page</a></li>
-							<li><a href="./contact.html">Contact Page</a></li>
-						</ul>
-					</li>
-					<li><a href="#">Blog</a></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
-
-
+      <header className="header-section">
+        <div className="header-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-2 text-center text-lg-left">
+                {/* <!-- logo --> */}
+                <a href="./index.html" className="site-logo">
+                  {/* // <img src = {../../assets/img/logo.png} alt="logo" /> */}
+                </a>
+              </div>
+              <div className="col-xl-6 col-lg-5">
+                <form className="header-search-form">
+                  <input type="text" placeholder="Search on divisima ...." />
+                  <button>
+                    {/* <i className="flaticon-search"></i> */}
+                    <FaSearch size='18px'/>
+                  </button>
+                </form>
+              </div>
+              <div className="col-xl-4 col-lg-5">
+                <div className="user-panel">
+                  <div className="up-item">
+                    {/* <i className="flaticon-profile"></i> */}
+                    <FaUser size='22px'/>
+                    {/* <FontAwesomeIcon icon="fa-light fa-user" /> */}
+                    <a href="#">Sign</a> In or <a href="#">Create Account</a>
+                  </div>
+                  <div className="up-item">
+                    <div className="shopping-card">
+                      <i className="flaticon-bag"></i>
+                      <FaCartPlus size= '22px'/>
+                      <span>0</span>
+                    </div>
+                    <a href="#">Shopping Cart</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <nav className="main-navbar">
+          <div className="container">
+            {/* <!-- menu --> */}
+            <ul className="main-menu">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Women</a>
+              </li>
+              <li>
+                <a href="#">Men</a>
+              </li>
+              <li>
+                <a href="#">
+                  Jewelry
+                  <span className="new">New</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">Shoes</a>
+                <ul className="sub-menu">
+                  <li>
+                    <a href="#">Sneakers</a>
+                  </li>
+                  <li>
+                    <a href="#">Sandals</a>
+                  </li>
+                  <li>
+                    <a href="#">Formal Shoes</a>
+                  </li>
+                  <li>
+                    <a href="#">Boots</a>
+                  </li>
+                  <li>
+                    <a href="#">Flip Flops</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Pages</a>
+                <ul className="sub-menu">
+                  <li>
+                    <a href="./product.html">Product Page</a>
+                  </li>
+                  <li>
+                    <a href="./category.html">Category Page</a>
+                  </li>
+                  <li>
+                    <a href="./cart.html">Cart Page</a>
+                  </li>
+                  <li>
+                    <a href="./checkout.html">Checkout Page</a>
+                  </li>
+                  <li>
+                    <a href="./contact.html">Contact Page</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
