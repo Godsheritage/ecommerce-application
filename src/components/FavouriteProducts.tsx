65f7a6e35b759a976139866c7aa55a5e7b78a523
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-17 17:51:34 +0100

    created favourite product components

---
+++ src/components/FavouriteProducts.tsx
@@ -0,0 +1,103 @@
+import product1 from "../assets/img/product/1.jpg";
+import product2 from "../assets/img/product/2.jpg";
+import product5 from "../assets/img/product/5.jpg";
+import product9 from "../assets/img/product/9.jpg";
+
+const FavouriteProducts = () => {
+  return (
+    <div>
+      <section className="related-product-section spad">
+        <div className="container">
+          <div className="section-title">
+            <h2>Your Favorites</h2>
+          </div>
+          <div className="row">
+            <div className="col-lg-3 col-sm-6">
+              <div className="product-item">
+                <div className="pi-pic">
+                  <div className="tag-new">New</div>
+                  <img src={product2} alt="product 2" />
+                  <div className="pi-links">
+                    <a href="#" className="add-card">
+                      <i className="flaticon-bag"></i>
+                      <span>ADD TO CART</span>
+                    </a>
+                    <a href="#" className="wishlist-btn">
+                      <i className="flaticon-heart"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="pi-text">
+                  <h6>$35,00</h6>
+                  <p>Black and White Stripes Dress</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="product-item">
+                <div className="pi-pic">
+                  <img src={product5} alt="product 5" />
+                  <div className="pi-links">
+                    <a href="#" className="add-card">
+                      <i className="flaticon-bag"></i>
+                      <span>ADD TO CART</span>
+                    </a>
+                    <a href="#" className="wishlist-btn">
+                      <i className="flaticon-heart"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="pi-text">
+                  <h6>$35,00</h6>
+                  <p>Flamboyant Pink Top </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="product-item">
+                <div className="pi-pic">
+                  <img src={product9} alt="product 9" />
+                  <div className="pi-links">
+                    <a href="#" className="add-card">
+                      <i className="flaticon-bag"></i>
+                      <span>ADD TO CART</span>
+                    </a>
+                    <a href="#" className="wishlist-btn">
+                      <i className="flaticon-heart"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="pi-text">
+                  <h6>$35,00</h6>
+                  <p>Flamboyant Pink Top </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="product-item">
+                <div className="pi-pic">
+                  <img src={product1} alt="product 1" />
+                  <div className="pi-links">
+                    <a href="#" className="add-card">
+                      <i className="flaticon-bag"></i>
+                      <span>ADD TO CART</span>
+                    </a>
+                    <a href="#" className="wishlist-btn">
+                      <i className="flaticon-heart"></i>
+                    </a>
+                  </div>
+                </div>
+                <div className="pi-text">
+                  <h6>$35,00</h6>
+                  <p>Flamboyant Pink Top </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default FavouriteProducts;
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,17 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
-
-
+import FavouriteProducts from "../components/FavouriteProducts";
 
 import cartImg1 from "../assets/img/cart/1.jpg";
 import cartImg2 from "../assets/img/cart/2.jpg";
 import cartImg3 from "../assets/img/cart/3.jpg";
 
-import product1 from "../assets/img/product/1.jpg";
-import product2 from "../assets/img/product/2.jpg";
-import product5 from "../assets/img/product/5.jpg";
-import product9 from "../assets/img/product/9.jpg";
+
 
 const Cart = () => {
   return (
@@ -137,100 +132,9 @@ const Cart = () => {
           </div>
         </div>
       </section>
-      {/* <!-- cart section end --> */}
 
-      {/* <!-- Related product section --> */}
-      <section className="related-product-section">
-        <div className="container">
-          <div className="section-title text-uppercase">
-            <h2>Continue Shopping</h2>
-          </div>
-          <div className="row">
-            <div className="col-lg-3 col-sm-6">
-              <div className="product-item">
-                <div className="pi-pic">
-                  <div className="tag-new">New</div>
-                  <img src={product2} alt="product 2" />
-                  <div className="pi-links">
-                    <a href="#" className="add-card">
-                      <i className="flaticon-bag"></i>
-                      <span>ADD TO CART</span>
-                    </a>
-                    <a href="#" className="wishlist-btn">
-                      <i className="flaticon-heart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div className="pi-text">
-                  <h6>$35,00</h6>
-                  <p>Black and White Stripes Dress</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-sm-6">
-              <div className="product-item">
-                <div className="pi-pic">
-                  <img src={product5} alt="product 5" />
-                  <div className="pi-links">
-                    <a href="#" className="add-card">
-                      <i className="flaticon-bag"></i>
-                      <span>ADD TO CART</span>
-                    </a>
-                    <a href="#" className="wishlist-btn">
-                      <i className="flaticon-heart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div className="pi-text">
-                  <h6>$35,00</h6>
-                  <p>Flamboyant Pink Top </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-sm-6">
-              <div className="product-item">
-                <div className="pi-pic">
-                  <img src={product9} alt="product 9" />
-                  <div className="pi-links">
-                    <a href="#" className="add-card">
-                      <i className="flaticon-bag"></i>
-                      <span>ADD TO CART</span>
-                    </a>
-                    <a href="#" className="wishlist-btn">
-                      <i className="flaticon-heart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div className="pi-text">
-                  <h6>$35,00</h6>
-                  <p>Flamboyant Pink Top </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-sm-6">
-              <div className="product-item">
-                <div className="pi-pic">
-                  <img src={product1} alt="product 1" />
-                  <div className="pi-links">
-                    <a href="#" className="add-card">
-                      <i className="flaticon-bag"></i>
-                      <span>ADD TO CART</span>
-                    </a>
-                    <a href="#" className="wishlist-btn">
-                      <i className="flaticon-heart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div className="pi-text">
-                  <h6>$35,00</h6>
-                  <p>Flamboyant Pink Top </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <Footer/>
+      <FavouriteProducts />
+      <Footer />
     </div>
   );
 };
