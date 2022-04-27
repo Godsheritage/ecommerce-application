1a21ce4238cd1ccfeda4f3a9f669897142d9f602
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-27 21:14:37 +0100

    added the latest product section

---
+++ client/src/components/Shared/ProductSection.tsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import { contextTypes } from "../../types";
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
+import ProductContext from "../../context/ProductContext";
+
+const ProductSection: React.FC = () => {
+
+  const { latestProducts, addToCart } = useContext(
+    ProductContext
+  ) as contextTypes;
+
+  return (
+    <div>
+      <section className="top-letest-product-section">
+        <div className="container">
+          <div className="section-title">
+            <h2>LATEST PRODUCTS</h2>
+          </div>
+          <div className="row">
+            {latestProducts.map((product) => (
+              <div className="col-lg-3 col-sm-6" key={product._id}>
+                <div className="product-item">
+                  <div className="pi-pic">
+                    <img src={product.image} alt={product.name} />
+                    <div className="pi-links">
+                      <button
+                        className="add-card"
+                        onClick={() => addToCart(product)}
+                      >
+                        <FaShoppingCart className="i" />
+                        <span>ADD TO CART</span>
+                      </button>
+                      <button className="wishlist-btn">
+                        <FaHeart className="i" />
+                      </button>
+                    </div>
+                  </div>
+                  <div className="pi-text">
+                    <h6>${product.price}</h6>
+                    <p>{product.name}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ProductSection;
