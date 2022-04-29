32d5836d34092521109cfe25b7fb64d388766d4a
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-29 05:34:19 +0100

    updates

--- server/dist/app.js
+++ server/dist/app.js
@@ -8,9 +8,13 @@ const path_1 = __importDefault(require("path"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
 const express_1 = __importDefault(require("express"));
+const passport_1 = __importDefault(require("passport"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const passport_google_oauth20_1 = require("passport-google-oauth20");
 const cart_routes_1 = __importDefault(require("./routes/Cart Routes/cart.routes"));
 const contact_routes_1 = __importDefault(require("./routes/contact routes/contact.routes"));
 const product_routes_1 = __importDefault(require("./routes/Product Routes/product.routes"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)("combined"));
@@ -19,11 +23,26 @@ app.use((0, helmet_1.default)());
 app.use("/cartitems", cart_routes_1.default);
 app.use("/Contact", contact_routes_1.default);
 app.use("/products", product_routes_1.default);
-app.get("/auth/google", (req, res) => {
-    console.log("logged");
-});
-app.get("/auth/google/callback", (req, res) => {
-    console.log("logged");
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+const AUTH_OPTIONS = {
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    callbackURL: "https://localhost:5000/auth/google/callback",
+};
+const verifyCallback = (accessToken, refreshToken, profile, done) => { };
+passport_1.default.use(new passport_google_oauth20_1.Strategy(AUTH_OPTIONS, verifyCallback));
+app.get("/auth/google", passport_1.default.authenticate("google", { scope: ["email", "profile"] }));
+app.get("/auth/google/callback", passport_1.default.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+}));
+app.get("failure", (req, res) => {
+    return res.status(400).json({
+        error: "Failed to login",
+    });
 });
 app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public ")));
 app.get("/*", (req, res) => {
--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,18 @@ import path from "path";
 import morgan from "morgan";
 import helmet from "helmet";
 import express from "express";
-import passport from "passport";
+import passport, { Profile, session } from "passport";
+import dotenv, { config } from "dotenv";
+import {
+  Strategy,
+  StrategyOptionsWithRequest,
+  VerifyCallback,
+} from "passport-google-oauth20";
 import cartRoute from "./routes/Cart Routes/cart.routes";
 import contactRoute from "./routes/contact routes/contact.routes";
 import productRoute from "./routes/Product Routes/product.routes";
+
+dotenv.config();
 
 const app = express();
 
@@ -18,12 +26,43 @@ app.use("/cartitems", cartRoute);
 app.use("/Contact", contactRoute);
 app.use("/products", productRoute);
 
-app.get("/auth/google", (req, res) => {
-  console.log("logged");
-});
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
-app.get("/auth/google/callback", (req, res) => {
-  console.log("logged");
+const AUTH_OPTIONS: any = {
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+  callbackURL: "https://localhost:5000/auth/google/callback",
+};
+
+const verifyCallback = (
+  accessToken: string,
+  refreshToken: string,
+  profile: Profile,
+  done: VerifyCallback
+) => {};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+  })
+);
+
+app.get("failure", (req, res) => {
+  return res.status(400).json({
+    error: "Failed to login",
+  });
 });
 
 app.use(express.static(path.join(__dirname, "..", "public ")));
