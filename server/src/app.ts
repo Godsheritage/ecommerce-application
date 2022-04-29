6386e84883450d2e8094892191919e88c06fe4c2
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-29 04:43:42 +0100

    trying to integrste oauth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import path from "path";
 import morgan from "morgan";
+import helmet from 'helmet';
 import express from "express";
 import cartRoute from "./routes/Cart Routes/cart.routes";
 import contactRoute from "./routes/contact routes/contact.routes";
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors())
 app.use(morgan("combined"));
 app.use(express.json());
+app.use(helmet())
 app.use("/cartitems", cartRoute);
 app.use("/Contact", contactRoute);
 app.use("/products", productRoute);
