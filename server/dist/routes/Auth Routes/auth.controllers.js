91109d995ecc11eb9a6e13434223d42878af7407
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-03 12:34:02 +0100

    working on the sign up logic

--- server/dist/routes/Auth Routes/auth.controllers.js
+++ server/dist/routes/Auth Routes/auth.controllers.js
@@ -3,5 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.httpSignup = void 0;
 const httpSignup = (req, res) => {
     const { email, password, confirmPassword } = req.body;
+    if (!email || !password || !confirmPassword) {
+        return res.status(400).json({
+            error: "missing fields",
+        });
+    }
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            error: "passwords don't match",
+        });
+    }
+    const checkIfMailExists = ;
 };
 exports.httpSignup = httpSignup;
--- server/src/routes/Auth Routes/auth.controllers.ts
+++ server/src/routes/Auth Routes/auth.controllers.ts
@@ -1,8 +1,19 @@
 import { RequestHandler } from "express";
 
-export const httpSignup:RequestHandler = (req, res) => {
-    const {email, password, confirmPassword} = req.body
+export const httpSignup: RequestHandler = (req, res) => {
+  const { email, password, confirmPassword } = req.body;
 
-    
+  if (!email || !password || !confirmPassword) {
+    return res.status(400).json({
+      error: "missing fields",
+    });
+  }
 
-}
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      error: "passwords don't match",
+    });
+  }
+
+  const checkIfMailExists = 
+};
